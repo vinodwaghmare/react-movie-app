9bfd40efcbeb6f1ad7f3bf958a9e7d6f1e8c5e31
Vinod <vinodwaghmare890@gmail.com>
2025-10-17 13:35:39 +0530

    Search -> Details Page Added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images : {
+    dangerouslyAllowSVG : true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      }
+    ]
+  },
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
+  devIndicators: {
+    position: 'bottom-right',
+  },  
 };
 
 export default nextConfig;
